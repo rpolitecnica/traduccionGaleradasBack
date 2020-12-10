0d05bfef197d89c39aac2784bb0c317e0110a3d7
sebastian <sebastian_toro24131@elpoli.edu.co>
2020-12-09 21:10:40 -0500

    cambios

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -6,12 +6,20 @@
     PORT:"8889"
   };*/
 
-
- module.exports = {
+//Base de datos primera
+ /*module.exports = {
     HOST: "us-cdbr-east-02.cleardb.com",
     USER: "b1c4668d8dd3df", 
     PASSWORD: "0124918b",
     DB: "heroku_d90a4abe03d2eed",
+  };*/
+
+//Base de datos segunda
+  module.exports = {
+    HOST: "us-cdbr-east-02.cleardb.com",
+    USER: "b2d76cfd4af793", 
+    PASSWORD: "3a896d50",
+    DB: "heroku_051d4e1e2aa9343",
   };
 
   //mysql://b1c4668d8dd3df:0124918b@us-cdbr-east-02.cleardb.com/heroku_d90a4abe03d2eed?reconnect=true
--- server.js
+++ server.js
@@ -411,7 +411,8 @@ app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
     //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
-    res.setHeader('Access-Control-Allow-Origin', 'https://traducciongaleradas.herokuapp.com');
+    //res.setHeader('Access-Control-Allow-Origin', 'https://traducciongaleradas.herokuapp.com');
+    res.setHeader('Access-Control-Allow-Origin', 'https://traducciongaleradasfront.herokuapp.com');
     
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
